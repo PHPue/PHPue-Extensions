180a7f8c206e1b3e450aa5e6efb8ed63e5f7b52d
Edward Patch <54278802+ShinkuKira21@users.noreply.github.com>
2025-11-25 12:25:44 +0000

    Update phpue-metacontrol.ext.php

--- phpue-metacontrol/backend/phpue-metacontrol.ext.php
+++ phpue-metacontrol/backend/phpue-metacontrol.ext.php
@@ -9,7 +9,7 @@
      * @author     Edward Patch
      * @license    PHPueExtensions Repo's Licence
      * @link       https://phpue.co.uk/
-     * @copyright  2025 Edward Patch
+     * @copyright  2025 Edward Patch (Apache 2 License)
      * 
      * @description
      * This extension provides a singleton MetaControl class that allows
@@ -25,55 +25,55 @@
 
     namespace PHPueExt;
 
-    if (defined('PHPUE_VERSION') && version_compare(PHPUE_VERSION, '0.0.1', '>=')) {
-        class MetaControl
-        {
-            private static ?self $instance = null;
-            private array $META_MAP;
-            private bool $MetaRendered = false;
+    if (!defined('PHPUE_VERSION') || version_compare(PHPUE_VERSION, '0.0.1', '<')) {
+        error_log("PHPue MetaControl: Incompatible framework version " . (PHPUE_VERSION ?? 'undefined'));
+    }
 
-            private function __construct() {
-                if (session_status() === PHP_SESSION_NONE) {
-                    session_start();
-                }
+    class MetaControl
+    {
+        private static ?self $instance = null;
+        private array $META_MAP;
+        private bool $MetaRendered = false;
 
-                $this->META_MAP = $_SESSION['PHPueExt_MetaMap'] ?? [];
-                $this->MetaRendered = $_SESSION['PHPueExt_MetaRendered'] ?? false;
+        private function __construct() {
+            if (session_status() === PHP_SESSION_NONE) {
+                session_start();
             }
 
-            public static function getInstance(): self {
-                if (self::$instance === null) {
-                    self::$instance = new self();
-                }
-                return self::$instance;
-            }
-
-            public function setMetaVariable(string $key, mixed $value): void {
-                $this->META_MAP[$key] = $value;
-                $_SESSION['PHPueExt_MetaMap'] = $this->META_MAP; 
-            }
-
-            public function getMetaVariable(string $key): mixed {
-                return $this->META_MAP[$key] ?? '';
-            }
-
-            public function unsetMetaVariables(): void {
-                unset($_SESSION['PHPueExt_MetaMap']);
-                $this->META_MAP = [];
-            }
-
-            public function setRendered(bool $rendered = true): void {
-                $this->MetaRendered = $rendered;
-                $_SESSION['PHPueExt_MetaRendered'] = $this->MetaRendered;
-            }
-
-            public function getRendered(): bool {
-                return $this->MetaRendered;
-            }
+            $this->META_MAP = $_SESSION['PHPueExt_MetaMap'] ?? [];
+            $this->MetaRendered = $_SESSION['PHPueExt_MetaRendered'] ?? false;
         }
 
-        \PHPueExt\MetaControl::getInstance();
-    } else {
-        error_log("META CONTROL DOESN'T SUPPORT YOUR VERSION OF PHPUE (Update to >= 0.0.1)");
+        public static function getInstance(): self {
+            if (self::$instance === null) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+
+        public function setMetaVariable(string $key, mixed $value): void {
+            $this->META_MAP[$key] = $value;
+            $_SESSION['PHPueExt_MetaMap'] = $this->META_MAP; 
+        }
+
+        public function getMetaVariable(string $key): mixed {
+            return $this->META_MAP[$key] ?? '';
+        }
+
+        public function unsetMetaVariables(): void {
+            unset($_SESSION['PHPueExt_MetaMap']);
+            $this->META_MAP = [];
+        }
+
+        public function setRendered(bool $rendered = true): void {
+            $this->MetaRendered = $rendered;
+            $_SESSION['PHPueExt_MetaRendered'] = $this->MetaRendered;
+        }
+
+        public function getRendered(): bool {
+            return $this->MetaRendered;
+        }
     }
+
+    \PHPueExt\MetaControl::getInstance();
 ?>
